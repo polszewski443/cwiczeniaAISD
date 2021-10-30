829abb42c469e08cb4f8a2dc31ec8461892ce738
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-31 00:43:38 +0200

    Add files via upload

---
+++ zad9.1.py
@@ -0,0 +1,12 @@
+daysOfWeek = {
+        "1": "Poniedzialek",
+        "2": "Wtorek",
+        "3": "Środa",
+        "4": "Czwartek",
+        "5": "Piątek",
+        "6": "Sobota",
+        "7": "Niedziela"
+        }
+
+a = input("\033[92mPodaj numer dnia tygodnia, a \033[93mprogram zwroci jego nazwe!: ")
+print(daysOfWeek[a])
